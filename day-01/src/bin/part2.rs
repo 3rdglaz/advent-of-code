f1865d91576c939a3992e665cdf46ae462018674
3rdglaz <pmscaio@gmail.com>
2023-12-02 16:06:02 -0300

    not working

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use day_01::part1::process;
+use day_01::part2::*;
 use miette::Context;
 
 #[cfg(feature = "dhat-heap")]
@@ -14,7 +14,7 @@ fn main() -> miette::Result<()> {
     tracing_subscriber::fmt::init();
 
     let file = include_str!("../input1.txt");
-    let result = process(file).context("proccess part 1")?;
+    let result = process(file).context("process part 2");
     println!("{}", result);
     Ok(())
 }
