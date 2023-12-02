cbb7dff18f49a658f5c382da2a1971a29d17bce4
3rdglaz <pmscaio@gmail.com>
2023-12-02 19:21:39 -0300

    change on part2

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -14,7 +14,7 @@ fn main() -> miette::Result<()> {
     tracing_subscriber::fmt::init();
 
     let file = include_str!("../input1.txt");
-    let result = process(file).context("process part 2");
+    let result = process(file).context("process part 2")?;
     println!("{}", result);
     Ok(())
 }
